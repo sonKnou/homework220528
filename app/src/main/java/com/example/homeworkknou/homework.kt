c40176805ac10dd44a0507f440c0269618d7e326
namhyun Kim <sonsory@gmail.com>
2022-05-28 18:09:00 +0900

    branch sonsory 99dan

--- app/src/main/java/com/example/homeworkknou/homework.kt
+++ app/src/main/java/com/example/homeworkknou/homework.kt
@@ -4,9 +4,9 @@ fun main(args: Array<String>){
     for (x in 1..9)
         for(y in 1..9)
             if(y !== 9) {
-                print("$x * $y = ${x * y} 이다")
+                print("$x * $y = ${x * y} 이 맞다")
             } else {
-                println("$x * $y = ${x * y} 이다")
+                println("$x * $y = ${x * y} 이 맞다")
             }
 }
 
